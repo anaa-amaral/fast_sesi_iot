d5194d314daffc8fb962783265cc0c3ca4342a25
Hisabel <hisabel_lippel@estudante.sesisenai.org.br>
2025-10-27 10:08:05 -0300

    acender/apagar led

--- S3/S3.ino
+++ S3/S3.ino
@@ -1,25 +1,12 @@
-<<<<<<< HEAD
+
 #include <WiFi.h>
 #include <PubSubClient.h>
-=======
- #include <WiFi.h>
-
- const String SSID = "FIESC_IOT_EDU";
- const String PASS = "8120gv08";
-
- void setup() {
-  Serial.begin(115200);
-  Serial.println("Conectando ao WiFI");
-  WiFi.begin(SSID,PASS);
-  while(WiFi.status() != WL_CONNECTED){
-    Serial.print(".");
-    delay(200);
-  }
-  Serial.println("\nConectado com sucesso!");
->>>>>>> 16d3aa6fdfd85ac42ce3bad40cc9bff4a927cc00
+#include <WiFi.h>
 
 WiFiClient client;
 PubSubClient mqtt(client);
+
+#define PINO_LED 2
 
 const String SSID = "FIESC_IOT_EDU";
 const String PASS = "8120gv08";
@@ -28,7 +15,8 @@ const String URL   = "test.mosquitto.org";
 const int PORT     = 1883;
 const String USR   = "";
 const String broker_PASS  = "";
-const String Topic = "DSM1";
+const String MyTopic = "HisabelGata";
+const String OtherTopic = "JuliaLinda";
 
 
 
@@ -50,14 +38,35 @@ void setup() {
     Serial.print(".");
     delay(200);
   }
+  pinMode(PINO_LED, OUTPUT);
+  mqtt.subscribe(MyTopic.c_str());
+  mqtt.setCallback(callback);
   Serial.println("\nConectado com sucesso ao broker!");
 }
 
 void loop() {
   String mensagem = "Hisabel: ";
-  mensagem += "Olá";
-
-  mqtt.publish(Topic.c_str(),mensagem.c_str());
+  if (Serial.available()>0){
+    mensagem += Serial.readStringUntil('\n');
+    mqtt.publish(OtherTopic.c_str(),mensagem.c_str());
+    Serial.println(mensagem);
+  }
   mqtt.loop();
   delay(1000);
 }
+
+void callback(char* topic, byte* payload, unsigned int lenght){
+  String mensagem = "";
+  for(int i = 0; i < lenght; i ++){
+    mensagem += (char)payload[i];
+  }
+  Serial.println("Recebido: ");
+  Serial.println(mensagem);
+  if(mensagem == "Julia: Acender"){
+    digitalWrite(PINO_LED, HIGH);
+  }else if(mensagem == "Julia: Apagar"){
+    digitalWrite(PINO_LED, LOW);
+  }
+}
+
+
