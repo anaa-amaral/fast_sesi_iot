0302821c87791b5bbc68fc9fd8bf890c9699c44d
Baumann001 <gustavo_baumann@estudante.sesisenai.org.br>
2025-10-27 10:15:43 -0300

    acender led

--- s1/s1.ino
+++ s1/s1.ino
@@ -1,45 +1,69 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 
-
 WiFiClient client;
 PubSubClient mqtt(client);
- const String SSID = "FIESC_IOT_EDU";
- const String PASS = "8120gv08";
 
+#define PINO_LED 2
+
+//constantes p/ broker
 const String URL = "test.mosquitto.org";
 const int PORT = 1883;
 const String USR = "";
 const String broker_PASS = "";
-const String Topic = "DSM1";
+const String MyTopic = "Gustavo_sla";
+const String OtherTopic = "Ana";
 
- void setup() {
+
+const String SSID = "FIESC_IOT_EDU";
+const String PASS = "8120gv08";
+
+void setup() {
+  pinMode(PINO_LED, OUTPUT); // define pino do LED como saída
   Serial.begin(115200);
-  Serial.println("Conectando ao WiFI");
+  Serial.print("Conectado ao Wi-Fi");
   WiFi.begin(SSID,PASS);
   while(WiFi.status() != WL_CONNECTED){
     Serial.print(".");
     delay(200);
   }
-  Serial.println("\nConectado com sucesso!");
-  Serial.println("Conectando ao broker");
-  mqtt.setServer(URL.c_str(), PORT);
+  Serial.print("\nConectado com sucesso!");
+  Serial.print("Conectando ao broker");
+  mqtt.setServer(URL.c_str(),PORT);
   while(!mqtt.connected()){
-    String ID = "S1_";
+    String ID = "Trem";
     ID += String(random(0xffff),HEX);
-
-    mqtt.connect(ID.c_str(),USR.c_str(), broker_PASS.c_str());
+    mqtt.connect(ID.c_str(),USR.c_str(),broker_PASS.c_str());
     Serial.print(".");
     delay(200);
+
   }
-  Serial.println("\nConectado com sucesso ao broker!");
+  mqtt.subscribe(MyTopic.c_str());
+  mqtt.setCallback(callback);
+  Serial.println("\nConecxao com sucesso ao broker!");
 }
 
 void loop() {
-  String mensagem = " Gustavo : ";
-  mensagem += "sla";
-
-  mqtt.publish(Topic.c_str(), mensagem.c_str());
+  String mensagem ="Gustavo: ";
+  if(Serial.available()>0){
+    mensagem += Serial.readStringUntil('\n');
+    mqtt.publish(OtherTopic.c_str(),mensagem.c_str());
+  }
   mqtt.loop();
   delay(1000);
+
+}
+
+void callback(char* topic, byte* payload, unsigned int length){
+  String mensagem = "";
+  for(int i = 0; i < length; i++){
+    mensagem += (char)payload[i];//acender led
+  }
+  Serial.print("Recebido: ");
+  Serial.println(mensagem);
+  if(mensagem == "Ana: Acender"){
+    digitalWrite(PINO_LED, HIGH);
+  }else if (mensagem == "Ana: Apagar");{
+    digitalWrite(PINO_LED, LOW);
+  }
 }
