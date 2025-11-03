8254e5e52a9325427421e676b75a128fda761362
Baumann001 <gustavo_baumann@estudante.sesisenai.org.br>
2025-11-03 10:04:37 -0300

    nova conexão

--- s1/s1.ino
+++ s1/s1.ino
@@ -1,4 +1,4 @@
-#include <WiFi.h>
+#include <WiFiClientSecure.h>
 #include <PubSubClient.h>
 
 WiFiClient client;
@@ -7,13 +7,18 @@ PubSubClient mqtt(client);
 #define PINO_LED 2
 
 //constantes p/ broker
-const String URL = "test.mosquitto.org";
-const int PORT = 1883;
+const String URL = "7aecec580ecf4e5cbac2d52b35eb85b9.s1.eu.hivemq.cloud";
+const int PORT = 8883;
 const String USR = "";
-const String broker_PASS = "";
+const String broker_user = "Placa-1-Gustavo";
+const String broker_PASS = "123456abX";
+
 const String MyTopic = "Gustavo_sla";
 const String OtherTopic = "Ana";
 
+const String MyTopic = "SA_S1_presenca1";
+
+const String MyTopic = "SA_S1_servo1";
 
 const String SSID = "FIESC_IOT_EDU";
 const String PASS = "8120gv08";
@@ -28,6 +33,7 @@ void setup() {
     delay(200);
   }
   Serial.print("\nConectado com sucesso!");
+  client.setInsecure();
   Serial.print("Conectando ao broker");
   mqtt.setServer(URL.c_str(),PORT);
   while(!mqtt.connected()){
@@ -63,7 +69,7 @@ void callback(char* topic, byte* payload, unsigned int length){
   Serial.println(mensagem);
   if(mensagem == "Ana: Acender"){
     digitalWrite(PINO_LED, HIGH);
-  }else if (mensagem == "Ana: Apagar");{
+  }else if (mensagem == "Ana: Apagar"){
     digitalWrite(PINO_LED, LOW);
   }
 }
