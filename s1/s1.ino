f674068442b02fc8ec9306204e8ba70188e300f2
Baumann001 <gustavo_baumann@estudante.sesisenai.org.br>
2025-11-14 09:44:45 -0300

    conexão de todos os tópico feitos

--- s1/s1.ino
+++ s1/s1.ino
@@ -1,98 +1,135 @@
+
 #include <WiFi.h>
 #include <WiFiClientSecure.h>
 #include <PubSubClient.h>
+#include <DHT.h>
+
+
+const byte LDR_PIN = 34;
+const byte LED_PIN = 19;    
+#define DHTPIN 4
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
 
 WiFiClientSecure client;
 PubSubClient mqtt(client);
 
-#define PINO_LED 2
 
-// HiveMQ Cloud
 const char* MQTT_SERVER = "7aecec580ecf4e5cbac2d52b35eb85b9.s1.eu.hivemq.cloud";
-const int MQTT_PORT = 8883;
-const char* MQTT_USER = "Placa-1-Gustavo";
-const char* MQTT_PASS = "123456abX";
+const int   MQTT_PORT   = 8883;
+const char* MQTT_USER   = "Placa-1-Gustavo";
+const char* MQTT_PASS   = "123456abX";
 
-// Wi-Fi
-const char* SSID = "FIESC_IOT_EDU";
+
+const char* SSID      = "FIESC_IOT_EDU";
 const char* WIFI_PASS = "8120gv08";
 
-// Tópicos MQTT
-const char* TOPICO_UMIDADE = "S1/umidade";
+
+const char* TOPICO_UMIDADE     = "S1/umidade";
 const char* TOPICO_TEMPERATURA = "S1/temperatura";
-const char* TOPICO_LED = "S1/iluminacao";
+const char* TOPICO_LED         = "S1/iluminacao";
+
 
 void callback(char* topic, byte* payload, unsigned int length) {
   String mensagem;
+
   for (int i = 0; i < length; i++) {
     mensagem += (char)payload[i];
   }
-  Serial.print("Recebido no tópico ");
+
+  Serial.print("Recebido em ");
   Serial.print(topic);
   Serial.print(": ");
   Serial.println(mensagem);
-
-  if (mensagem == "acender") {
-    digitalWrite(PINO_LED, HIGH);
-    Serial.println("LED aceso!");
-  } else if (mensagem == "apagar") {
-    digitalWrite(PINO_LED, LOW);
-    Serial.println("LED apagado!");
+  if(strcmp(topic,TOPICO_LED) == 0){
+    if (mensagem == "acender") {
+      digitalWrite(LED_PIN, HIGH);
+    } else if (mensagem == "apagar") {
+      digitalWrite(LED_PIN, LOW);
+    }
   }
 }
 
+
+void conectaMQTT() {
+  while (!mqtt.connected()) {
+    Serial.print("Conectando ao HiveMQ... ");
+
+    String clientId = "ESP32-S1-";
+    clientId += String(random(0xffff), HEX);
+
+    if (mqtt.connect(clientId.c_str(), MQTT_USER, MQTT_PASS)) {
+      Serial.println("Conectado!");
+      mqtt.subscribe(TOPICO_LED);
+    } else {
+      Serial.println("Falhou. Tentando novamente...");
+      delay(1000);
+    }
+  }
+}
+
+
 void setup() {
-  pinMode(PINO_LED, OUTPUT);
   Serial.begin(115200);
 
-  Serial.print("Conectando ao Wi-Fi...");
+  pinMode(LED_PIN, OUTPUT);
+  dht.begin();
+
+  
+  Serial.print("Conectando ao Wi-Fi ");
   WiFi.begin(SSID, WIFI_PASS);
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(500);
   }
-  Serial.println(" conectado!");
+  Serial.println(" Conectado!");
 
-  client.setInsecure(); // sem certificado
+  client.setInsecure();
   mqtt.setServer(MQTT_SERVER, MQTT_PORT);
   mqtt.setCallback(callback);
-
-  Serial.print("Conectando ao HiveMQ...");
-  while (!mqtt.connected()) {
-    String clientId = "ESP32-S1-";
-    clientId += String(random(0xffff), HEX);
-
-    if (mqtt.connect(clientId.c_str(), MQTT_USER, MQTT_PASS)) {
-      Serial.println(" conectado!");
-    } else {
-      Serial.print(".");
-      delay(1000);
-    }
-  }
-
-  mqtt.subscribe(TOPICO_LED);
-  Serial.println("Assinado no tópico de LED.");
+  conectaMQTT();
 }
 
+
 void loop() {
+
   if (!mqtt.connected()) {
-    mqtt.connect("ESP32-S1", MQTT_USER, MQTT_PASS);
-    mqtt.subscribe(TOPICO_LED);
+    conectaMQTT();
   }
   mqtt.loop();
 
-  // Publica valores simulados
-  int umidade = random(40, 70);
-  int temperatura = random(20, 30);
+ 
+  int leituraLDR = analogRead(LDR_PIN);
+  Serial.print("LDR: ");
+  Serial.println(leituraLDR);
 
-  String msgUmidade = String(umidade);
-  String msgTemperatura = String(temperatura);
+  if (leituraLDR > 3500) {
+    Serial.println("acender");
+    digitalWrite(LED_PIN, HIGH);
+    mqtt.publish(TOPICO_LED, "acender");
+  } else {
+    Serial.println("apagar");
+    digitalWrite(LED_PIN, LOW);
+    mqtt.publish(TOPICO_LED, "apagar");
+  }
 
-  mqtt.publish(TOPICO_UMIDADE, msgUmidade.c_str());
-  mqtt.publish(TOPICO_TEMPERATURA, msgTemperatura.c_str());
+  
+  float umidade = dht.readHumidity();
+  float temperatura = dht.readTemperature();
+s
+  Serial.print("Temperatura: ");
+  Serial.print(temperatura);
+  Serial.println(" °C");
 
-  Serial.println("Publicado:");
-  Serial.println("  Umidade: " + msgUmidade);
-  Serial.println("  Temperatura: " + msgTemperatura);
-  delay(5000);
+  Serial.print("Umidade: ");
+  Serial.print(umidade);
+  Serial.println(" %");
+
+  mqtt.publish(TOPICO_TEMPERATURA, String(temperatura).c_str());
+  mqtt.publish(TOPICO_UMIDADE, String(umidade).c_str());
+
+  Serial.println("--------------------");
+
+  delay(1500);
 }
