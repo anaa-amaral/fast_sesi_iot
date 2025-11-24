846d053632e7e1504ba65e80fd8e74920c85b1e8
Julia Izabel <julia_roza@estudante.sesisenai.org.br>
2025-11-24 08:14:54 -0300

    Código comentado

--- S2/S2.ino
+++ S2/S2.ino
@@ -1,7 +1,8 @@
-#include <WiFi.h>
-#include <WiFiClientSecure.h>
+                                                                                                                                                                                                                                                                                                                         #include <WiFi.h>
+#include <WiFiClientSecure.h> //Permite a conexão segura com o broker MQTT
 #include <PubSubClient.h>
 
+//---Conexão com wifi e o broker--
 const char* SSID = "FIESC_IOT_EDU";
 const char* PASS = "8120gv08";
 
@@ -11,6 +12,7 @@ const int PORT = 8883;
 const char* BROKER_USER = "Placa-2-Julia";
 const char* BROKER_PASS = "123456abX";
 
+//Declara os pinos
 #define TRIG1 12
 #define ECHO1 25
 #define TRIG2 18
@@ -19,38 +21,38 @@ const char* BROKER_PASS = "123456abX";
 #define PINO_LED 4
 
 const char* TOPICO_PUBLISH_1 = "Projeto/S2/Distancia1";
-const char* TOPICO_PUBLISH_2 = "Projeto/S2/Distancia2";
-const char* TOPICO_SUBSCRIBE = "S1/iluminacao";
-const char* TOPICO_ENVIO_S3   = "Projeto/S3/Controle";
+const char* TOPICO_PUBLISH_2 = "Projeto/S2/Distancia2";//Publico as duas distâncias (status dos sensores)
+const char* TOPICO_SUBSCRIBE = "S1/iluminacao";//recebo o comando do S1
+const char* TOPICO_ENVIO_S3   = "Projeto/S3/Controle";//Envia esse status para o S3
 
 WiFiClientSecure espClient;
-PubSubClient mqtt(espClient);
+PubSubClient mqtt(espClient);//Torna mais seguro 
 
-long medirDistancia(int trigPin, int echoPin) {
-  digitalWrite(trigPin, LOW);
+long medirDistancia(int trigPin, int echoPin) {//dois pinos para medir a distância
+  digitalWrite(trigPin, LOW);// Envia um pulso curto para iniciar a medição
   delayMicroseconds(5);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-  long duracao = pulseIn(echoPin, HIGH, 30000);
-  long distancia = duracao * 0.034 / 2;
+  long duracao = pulseIn(echoPin, HIGH, 30000);//medir a duração em microssegundos
+  long distancia = duracao * 0.034 / 2;//Calcula a distância em centímetros
   return distancia;
 }
 
-void callback(char* topic, byte* payload, unsigned int length) {
+void callback(char* topic, byte* payload, unsigned int length) {//Função acionada automaticamente quando uma mensagem chega em um tópico assinado
   String mensagem;
   for (int i = 0; i < length; i++) {
-    mensagem += (char)payload[i];
+    mensagem += (char)payload[i];//basicamente converte os elemnetos da array para string de texto
   }
-  if (mensagem == "acender") {
+  if (mensagem == "acender") {//se a mensagem recebida for acender o pino acende 
     digitalWrite(PINO_LED, HIGH);
-  } else if (mensagem == "apagar") {
+  } else if (mensagem == "apagar") {//se a mensagem recebida for apagar o pino apaga
     digitalWrite(PINO_LED, LOW);
   }
-  Serial.println(mensagem);
+  Serial.println(mensagem);//Mostra a mensagem
 }
 
-void conectaWiFi() {
+void conectaWiFi() {//Conexão com o wifi
   WiFi.begin(SSID, PASS);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -58,7 +60,7 @@ void conectaWiFi() {
   Serial.println("Conectado ao WiFi!");
 }
 
-void conectaMQTT() {
+void conectaMQTT() {//Conexão com o broker
   mqtt.setServer(BROKER, PORT);
   mqtt.setCallback(callback);
   espClient.setInsecure();
@@ -73,7 +75,7 @@ void conectaMQTT() {
   Serial.println("Conectado ao broker!");
 }
 
-void setup() {
+void setup() {//Define os pinos como entrada e saída
   Serial.begin(115200);
   pinMode(TRIG1, OUTPUT);
   pinMode(ECHO1, INPUT);
@@ -86,10 +88,10 @@ void setup() {
 }
 
 void loop() {
-  if (!mqtt.connected()) {
+  if (!mqtt.connected()) {//Verifica se a conexão MQTT foi perdida e tenta reconectar
     conectaMQTT();
   }
-  long dist1 = medirDistancia(TRIG1, ECHO1);
+  long dist1 = medirDistancia(TRIG1, ECHO1);//Mede as duas distâncias
   long dist2 = medirDistancia(TRIG2, ECHO2);
   String msg1 = String(dist1);
   String msg2 = String(dist2);
@@ -98,11 +100,11 @@ void loop() {
   Serial.println(dist2);
 
   if (dist1 < 10) {
-    mqtt.publish(TOPICO_PUBLISH_1, "objeto_proximo");
+    mqtt.publish(TOPICO_PUBLISH_1, "objeto_proximo");//Se a ditância for menor que 10 ele publica objeto_proximo
     mqtt.publish(TOPICO_ENVIO_S3, "objeto_proximo");
   }
   if(dist2 > 10) {
-    mqtt.publish(TOPICO_PUBLISH_2, "objeto_longe");
+    mqtt.publish(TOPICO_PUBLISH_2, "objeto_longe");//Se a ditância for maior que 10 ele publica objeto_longe
     mqtt.publish(TOPICO_ENVIO_S3, "objeto_longe");
   }
   
